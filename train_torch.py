015009bb99c224b75cdb1df92006e646d6df8a53
Yanggum <bak405@naver.com>
2022-09-25 14:26:50 +0900

    Update train_torch.py

--- train_torch.py
+++ train_torch.py
@@ -228,7 +228,10 @@ if __name__ == "__main__":
         checkpoint_callback = ModelCheckpoint(
             dirpath='model_chp',
             filename='{epoch:02d}-{train_loss:.2f}',
-            monitor='train_loss'
+            verbose=True,
+            save_last=True,
+            monitor='train_loss',
+            mode='min'
         )
         # python train_torch.py --train --gpus 1 --max_epochs 3
         model = KoGPT2Chat(args)
